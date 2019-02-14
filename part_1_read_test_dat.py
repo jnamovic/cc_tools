2e4301bea15d31fd4e69d9f5cd5cdb48c44f9af4
jnamovic <jnamovic@andrew.cmu.edu>
2019-02-13 21:33:09 -0500

    please dont be broken

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -1,7 +1,11 @@
 import cc_dat_utils
 
 #Part 1
-input_dat_file = "data/pfgd_test.dat"
+input_dat_file = "C:/Users/Jonathan Namovic/Documents/Game design/Chips Challenge/cc_tools/data/pfgd_test.dat"
 
 #Use cc_dat_utils.make_cc_data_from_dat() to load the file specified by input_dat_file
 #print the resulting data
+
+gameMap=cc_dat_utils.make_cc_data_from_dat(input_dat_file)
+
+print(gameMap)
